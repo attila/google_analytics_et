04e76fad2e020fd585fc7973c9fcdc4190c6d14a
Frank Anderson <FrankAnderson@Frank-Andersons-Mac-mini.local>
2011-11-18 14:28:22 -0800

    rewrote the javascirpt to use the each and bind methodes to fix the bug of only the last event binded being executed on every event. Also, added the ability to use a token to do a replace of the String with the output of the text() methode.

--- js/gaet.js
+++ js/gaet.js
@@ -6,27 +6,30 @@ jQuery(document).ready(function () {
 	}
 	var settings = Drupal.settings.google_analytics_event_tracking_settings;
 	
-	
-	// Primary Navigation
-//	$('#prinav > li > a').mousedown(
-	//	function(evt){
-	//		_gaq.push(['_trackEvent', 'Primary Nav', 'Click', $(this).text()]);
-//		}
-//	);
-	
-	
-	
+	var s = new Array();
 	for(var i = 0; i < settings.selectors.length; i++) {
-		selector = settings.selectors[i];
-		jQuery(selector.selector).mousedown(
-			function(e) {
-				trackEvent(jQuery(this), selector.category, selector.action, selector.label, selector.value, selector.noninteraction)
-			}
-		);
+		s[i] = settings.selectors[i].selector;
 	}
+	
+	jQuery.each(s,
+		function(i, val) {
+			jQuery(settings.selectors[i].selector).bind('mousedown',
+				function(event) {
+					trackEvent(jQuery(this), settings.selectors[i].category, settings.selectors[i].action, settings.selectors[i].label, settings.selectors[i].value, settings.selectors[i].noninteraction)
+				}
+			);
+		}
+	);
 	
 });
 
 function trackEvent($obj, category, action, opt_label, opt_value, opt_noninteraction) {
+	category = category == '!text' ? String($obj.text()) : String(category);
+	action = action == '!text' ? String($obj.text()) : String(action);
+	opt_label = opt_label == '!text' ? String($obj.text()) : String(opt_label);
 	_gaq.push(['_trackEvent', String(category), String(action), String(opt_label), Number(opt_value), Boolean(opt_noninteraction)]);
+}
+
+function debugEvent($obj, category, action, opt_label, opt_value, opt_noninteraction) {
+	alert(category + ' ' + action  + ' ' + opt_label + ' ' + opt_value);
 }
